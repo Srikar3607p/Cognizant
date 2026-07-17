ac64d93924bb851f2dd14af4fa94eb55d4f05038
Srikar3607p <srikar3607@gmail.com>
2026-07-17 23:48:19 +0530

    CTS complete excercises

--- deepskill/Week-3/StudentAPI/DTOs/StudentRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentAPI.DTOs
-{
-    public class StudentRequest
-    {
-        public string Name { get; set; } = "";
-        public int Age { get; set; }
-        public string Department { get; set; } = "";
-    }
-}
